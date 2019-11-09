3e06d0bdf8aac35b55242c3b8e3a1c7edf863dec
Khalid Boulssane <khalid.boulssane@hotmail.com>
2019-11-09 15:24:51 +0100

    Utilisation de leaflet pour generer la map

---
+++ js/leaflet.js
@@ -0,0 +1,10 @@
+//Nous allons utiliser la geolocalisation du navigateur
+navigator.geolocation.getCurrentPosition(function(position){
+	//Generation de la map mais avec fond gris
+	var mymap = L.map('map').setView([position.coords.latitude, position.coords.longitude], 11);
+
+	//mettons lui un fond -> un tileLayer 
+	L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',{
+	maxZoom:20,
+	}).addTo(mymap);
+});
