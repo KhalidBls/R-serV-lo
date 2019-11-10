a45761ae936208eec7cf0129905813a0a9dc136d
Khalid Boulssane <khalid.boulssane@hotmail.com>
2019-11-10 21:13:03 +0100

    Début du slider en javascript

--- js/leaflet.js
+++ js/leaflet.js
@@ -8,3 +8,36 @@ navigator.geolocation.getCurrentPosition(function(position){
 	maxZoom:20,
 	}).addTo(mymap);
 });
+
+//le slideeeeeeer************************************************
+
+
+let count = 1;		//Le counter pour modifier la valeur de 630
+
+//On cible le slides (il y a les images dedans)
+var slides = document.getElementById("slides");
+
+//On fait un tableau avec nos images dedans
+var imgSlider = document.getElementsByClassName("img-slider");
+
+//On cible nos flèches
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+// Test au clique du bouton
+
+slides.style.transform="translateX("+ (-630 * count) +"px)";
+
+nextBtn.addEventListener("click", function(){
+	slides.style.transition="transform 0.4s ease-in-out";
+	count++;
+	slides.style.transform="translateX("+ (-630 * count) +"px)";
+	
+});
+
+prevBtn.addEventListener("click", function(){
+	slides.style.transition="transform 0.4s ease-in-out";
+	count--;
+	slides.style.transform="translateX("+ (-630 * count) +"px)";
+});
+
