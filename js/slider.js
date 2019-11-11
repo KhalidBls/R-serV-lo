4303d36b1755faa9b21cfb1eb6bdde9ce2069f34
Khalid Boulssane <khalid.boulssane@hotmail.com>
2019-11-11 14:13:24 +0100

    Ajout de la methode infinite

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-//le slideeeeeeer************************************************
+//****************le slideeeeeeer   (TEST DE L'OBJET EN JS)********************
 
 
 class SlideTest {
@@ -6,7 +6,7 @@ class SlideTest {
 	constructor(){
 	this.count = 1,														//Le counter pour modifier la valeur de 630
 	this.slidesT = document.getElementById("slides");					//On cible le slides (il y a les images dedans)
-	//imgSlider : document.getElementsByClassName("img-slider"),		//On fait un tableau avec nos images dedans
+	this.imgSlider = document.getElementsByClassName("img-slider"),		//On fait un tableau avec nos images dedans
 	this.prevBtn = document.querySelector("#prevBtn");		//On cible nos flèches
 	this.nextBtn = document.querySelector("#nextBtn");
 	}
@@ -20,13 +20,28 @@ class SlideTest {
 		this.count--;
 		this.slidesT.style.transform="translateX("+ (-630 * this.count) +"px)";
 	}
+	infinite(){
+		if(this.imgSlider[this.count].id === "lastClone"){     	"Si l'id de l'image en cours est \"lastClone"
+			this.slidesT.style.transition = "none";					"On enlève la transition si non effet très NUL"
+			this.count = this.imgSlider.length - 2;				"Pour revenir à la première image (A REVOIR SA)"
+			this.slidesT.style.transform="translateX("+ (-630 * this.count) +"px)"; "Pour le translate dans le bon sens"
+		}
+	 	if(this.imgSlider[this.count].id === "firstClone"){    
+			this.slidesT.style.transition = "none";					
+			this.count = this.imgSlider.length - this.count;				"Pour revenir à la première image (A REVOIR SA)"
+			this.slidesT.style.transform="translateX("+ (-630 * this.count) +"px)"; "Pour le translate dans le bon sens"
+		}
+	}
 }
 
-// le code commence ici 
+
+
+// le code commence ici   une sorte de main()  où on va appeler nos methodes
 
 const monSlide = new SlideTest();
 
 monSlide.slidesT.style.transform="translateX("+ (-630 * monSlide.count) +"px)";
-
 monSlide.nextBtn.addEventListener("click", function(){monSlide.moveRight()});
 monSlide.prevBtn.addEventListener("click", function(){monSlide.moveLeft()});
+// faire un slider "illimités"
+monSlide.slidesT.addEventListener("transitionend",function(){monSlide.infinite()});
