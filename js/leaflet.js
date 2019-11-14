d4f0eb223cd9a9190878402ede33f92bd54717ca
Khalid Boulssane <khalid.boulssane@hotmail.com>
2019-11-14 15:51:08 +0100

    Début de l'objet pour la carte

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,11 +1,51 @@
-function generateMap(position){
-	//Generation de la map mais avec fond gris
-	var mymap = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+class Carte {
+	constructor(){
+		
 
-	//mettons lui un fond -> un tileLayer 
-	L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',{
-	maxZoom:20,
-	}).addTo(mymap);
+
+
+		navigator.geolocation.getCurrentPosition(this.generateMap);
+	}
+	
+	generateMap(position){
+		var mymap=L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',{maxZoom:20,}).addTo(mymap);
+		ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=cergy-pontoise&apiKey=bde09ea11fef33327232c69bebab6569d6b275fd",function(reponse)
+	{
+		var stations=JSON.parse(reponse); // oN CONVERTIT LA REPONSE EN OBJET js
+		
+		for(var i = 0;i<stations.length;i++)  // Pour chaque station de velo on met sa position sur la map
+		{
+			//********************** Le texte du cadre
+			var text = "<b>Disponible</b> </br>" + stations[i].address;
+			text+= "</br>Places : " + stations[i].bike_stands;
+			text+="</br> Vélos disponibles : " + stations[i].available_bikes;
+			//*******************************  
+
+			var positionStation = L.marker([stations[i].position.lat,stations[i].position.lng]).addTo(mymap);
+
+			// SI L'ETAT DE LA STATION EST "open"  
+			if(stations[i].status === "OPEN"){
+			var button=document.createElement("button");
+			button.textContent="Réserver un vélo";
+			positionStation.bindPopup(text + "</br></br>" +button.outerHTML);
+
+			button.addEventListener("click",function(){alert("ok");});
+			}
+			else {positionStation.bindPopup("<b>Indisponible</b> </br>" + stations[i].address);}
+		}
+			
+	});
+	}
+
+}
+
+
+
+
+/********************************************************************************************************* 
+
+
 
 	// UTILISON L'API JCDECAUX
 	//ajaxGet  ----> fonction dans le fichier ajax.js
@@ -15,11 +55,11 @@ function generateMap(position){
 		
 		for(var i = 0;i<stations.length;i++)  // Pour chaque station de velo on met sa position sur la map
 		{
-			//********************** Le texte du cadre*/
+			//********************** Le texte du cadre
 			var text = "<b>Disponible</b> </br>" + stations[i].address;
 			text+= "</br>Places : " + stations[i].bike_stands;
 			text+="</br> Vélos disponibles : " + stations[i].available_bikes;
-			//*******************************  */
+			//*******************************  
 
 			var positionStation = L.marker([stations[i].position.lat,stations[i].position.lng]).addTo(mymap);
 
@@ -37,8 +77,4 @@ function generateMap(position){
 	});
 }
 
-//APPEL DE LA FONCTION :
-
-//Nous allons utiliser la geolocalisation du navigateur
-navigator.geolocation.getCurrentPosition(generateMap);
-
+*/
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 // le code commence ici   une sorte de main()  où on va appeler nos methodes
 
 const monSlide = new Slider(); //instanciation de la classe SlideTest
+const maCarte = new Carte();
