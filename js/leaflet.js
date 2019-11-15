dcc15a1e51c65678acbc3813f5f71df6f5e43920
Khalid Boulssane <khalid.boulssane@hotmail.com>
2019-11-15 12:53:31 +0100

    Création d'un panneau a coter de la carte

--- js/leaflet.js
+++ js/leaflet.js
@@ -2,79 +2,43 @@ class Carte {
 	constructor(){
 		
 
-
-
 		navigator.geolocation.getCurrentPosition(this.generateMap);
 	}
 	
 	generateMap(position){
-		var mymap=L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
-		L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',{maxZoom:20,}).addTo(mymap);
-		ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=cergy-pontoise&apiKey=bde09ea11fef33327232c69bebab6569d6b275fd",function(reponse)
-	{
-		var stations=JSON.parse(reponse); // oN CONVERTIT LA REPONSE EN OBJET js
 		
-		for(var i = 0;i<stations.length;i++)  // Pour chaque station de velo on met sa position sur la map
+		var mymap=L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png',{maxZoom:20}).addTo(mymap);
+
+		ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=cergy-pontoise&apiKey=bde09ea11fef33327232c69bebab6569d6b275fd",function(reponse)
 		{
-			//********************** Le texte du cadre
-			var text = "<b>Disponible</b> </br>" + stations[i].address;
-			text+= "</br>Places : " + stations[i].bike_stands;
-			text+="</br> Vélos disponibles : " + stations[i].available_bikes;
-			//*******************************  
+			var mappy = document.getElementById("mappy");
+			var carte = document.getElementById("map");
+			var content = document.createElement("div");
+			
+			var stations=JSON.parse(reponse); // oN CONVERTIT LA REPONSE EN OBJET js
 
-			var positionStation = L.marker([stations[i].position.lat,stations[i].position.lng]).addTo(mymap);
+			for(var i = 0;i<stations.length;i++)  // Pour chaque station de velo on met sa position sur la map
+			{
+				
+				 var positionStation= L.marker([stations[i].position.lat,stations[i].position.lng]).addTo(mymap);
 
-			// SI L'ETAT DE LA STATION EST "open"  
-			if(stations[i].status === "OPEN"){
-			var button=document.createElement("button");
-			button.textContent="Réserver un vélo";
-			positionStation.bindPopup(text + "</br></br>" +button.outerHTML);
 
-			button.addEventListener("click",function(){alert("ok");});
+
+						positionStation.addEventListener("click",function(){
+							
+							carte.style.transition="transform 0.4s ease-in-out";
+							carte.style.transform="translateX(-30px)";
+
+							mappy.style.display="flex";
+							mappy.appendChild(content);
+							content.style.width="320px";
+							content.style.backgroundColor="black";
+							content.style.borderRadius="2%";
+						});
 			}
-			else {positionStation.bindPopup("<b>Indisponible</b> </br>" + stations[i].address);}
-		}
 			
 	});
 	}
-
+	
 }
-
-
-
-
-/********************************************************************************************************* 
-
-
-
-	// UTILISON L'API JCDECAUX
-	//ajaxGet  ----> fonction dans le fichier ajax.js
-	ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=cergy-pontoise&apiKey=bde09ea11fef33327232c69bebab6569d6b275fd",function(reponse)
-	{
-		var stations=JSON.parse(reponse); // oN CONVERTIT LA REPONSE EN OBJET js
-		
-		for(var i = 0;i<stations.length;i++)  // Pour chaque station de velo on met sa position sur la map
-		{
-			//********************** Le texte du cadre
-			var text = "<b>Disponible</b> </br>" + stations[i].address;
-			text+= "</br>Places : " + stations[i].bike_stands;
-			text+="</br> Vélos disponibles : " + stations[i].available_bikes;
-			//*******************************  
-
-			var positionStation = L.marker([stations[i].position.lat,stations[i].position.lng]).addTo(mymap);
-
-			// SI L'ETAT DE LA STATION EST "open"  
-			if(stations[i].status === "OPEN"){
-			var button=document.createElement("button");
-			button.textContent="Réserver un vélo";
-			positionStation.bindPopup(text + "</br></br>" +button.outerHTML);
-
-			button.addEventListener("click",function(){alert("ok");});
-			}
-			else {positionStation.bindPopup("<b>Indisponible</b> </br>" + stations[i].address);}
-		}
-			
-	});
-}
-
-*/
