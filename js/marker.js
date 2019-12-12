7a8283b6651ab6e893da447b67ffe1da8fe06a7a
Khalid Boulssane <khalid.boulssane@hotmail.com>
2019-12-12 12:13:10 +0100

    objet reservation creer

--- js/marker.js
+++ js/marker.js
@@ -6,13 +6,8 @@ class Marker {
         this.mappy = document.getElementById("mappy");
         this.content = document.createElement("div");
         this.content.id = "content";
-        this.timer=document.createElement("p");
-        this.monStockageL = localStorage;
-        this.monStockageS = sessionStorage;
-
-        this.infoBlock = document.createElement("div");
-        this.infoBlock.id = "infoBlock"; 
-        this.test();
+ 
+        this.reservation = new Reservation();
     }
 
     coordMarker(){
@@ -58,64 +53,8 @@ class Marker {
         this.content.innerHTML+="<br><br>Signature : <br>";
         this.content.appendChild(personne.canvas);
         this.content.appendChild(personne.finalButton);
-        personne.finalButton.addEventListener("click",function(){this.infoReservation(stations)}.bind(this));
-        if(this.monStockageL) this.preremplir();
+        personne.finalButton.addEventListener("click",function(){this.reservation.infoReservation(stations)}.bind(this));
+        if(this.reservation.monStockageL) this.reservation.preremplir();
     }
-
-    infoReservation(stations){
-        clearInterval(this.decompte);
-        this.monStockageS.clear();
-        var prenom = document.querySelectorAll("input")[0].value;
-        var nom = document.querySelectorAll("input")[1].value;
-
-        this.monStockageL.setItem('prenom',prenom);
-        this.monStockageL.setItem('nom',nom);
-
-        this.minute=20;
-        this.sec=0;
-        this.timer.innerText = this.minute + "min " + this.sec + "s";
-
-        if(document.body.querySelector("#infoBlock")) document.body.removeChild(document.body.querySelector("#infoBlock"));
-        document.body.insertBefore(this.infoBlock,document.querySelector("footer"));
-        this.infoBlock.innerHTML = "<b>Votre Réservation</b><br>"
-        this.infoBlock.innerHTML += "Vélo Réservé à la station "+ stations.address;
-        this.infoBlock.innerHTML += " au nom de " + "<b>"+prenom + " " + nom +"</b>";
-        this.infoBlock.innerHTML +="<br><b>Temps restants: </b><br>";
-        this.infoBlock.appendChild(this.timer);
-        this.chrono(1200);
-
-        this.monStockageS.setItem('address',stations.address);
-    }
-
-    chrono(duree){
-        
-        this.decompte = setInterval(function(){
-            if(duree<=0){this.monStockageS.setItem('statusReservation','false');}
-            if(duree>0){this.monStockageS.setItem('statusReservation','true');}
-            this.minute=Math.floor(duree/60);
-            this.sec=Math.floor(duree%60);
-            duree--;
-            this.timer.innerText = this.minute + "min " + this.sec + "s";
-            this.monStockageS.setItem('duree',duree);
-        }.bind(this),1000);
-    }
-
-    preremplir(){
-        document.querySelectorAll("input")[0].value = this.monStockageL.getItem('prenom');
-        document.querySelectorAll("input")[1].value = this.monStockageL.getItem('nom');
-    }
-    
-    test(){
-        if(this.monStockageS.getItem('duree')<1200 && this.monStockageS.getItem('statusReservation')==='true' && !document.querySelector("#infoBlock"))
-        {
-            document.body.insertBefore(this.infoBlock,document.querySelector("footer"));
-            this.infoBlock.innerHTML = "<b>Votre Réservation</b><br>"
-            this.infoBlock.innerHTML += "Vélo Réservé à la station "+ this.monStockageS.getItem('address');
-            this.infoBlock.innerHTML += " au nom de " + "<b>"+this.monStockageL.getItem('prenom')+ " " + this.monStockageL.getItem('nom') +"</b>";
-            this.infoBlock.innerHTML +="<br><b>Temps restants: </b><br>";
-            this.infoBlock.appendChild(this.timer);
-            this.chrono(this.monStockageS.getItem('duree'));
-        }
-    }
-
-}
+} 
+//METTRE LES CLASSE DU CHRONOS DANS UNE CLASSE A PART
